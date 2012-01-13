3f0c194e522ce027ff203c6b411366408d636e3f
wren gayle romano <wren@community.haskell.org>
2012-01-13 00:58:28 -0500

    Math.Combinatorics.Binomial: more micro-optimizing
    Ignore-this: 9db1e21b2f4658ab93ef84682c852b27
    
    darcs-hash:19fecea65678adbd145dba2d12932bc31481d38f

--- src/Math/Combinatorics/Binomial.hs
+++ src/Math/Combinatorics/Binomial.hs
@@ -1,6 +1,6 @@
 {-# OPTIONS_GHC -Wall -fwarn-tabs #-}
 ----------------------------------------------------------------
---                                                    2011.12.14
+--                                                    2012.01.12
 -- |
 -- Module      :  Math.Combinatorics.Binomial
 -- Copyright   :  Copyright (c) 2011 wren ng thornton
@@ -23,7 +23,8 @@ import Math.Combinatorics.Primes    (primes)
 
 Some identities, but not really material for RULES:
     n `choose` 0     = 1
-    n `choose` n     = 1
+    n `choose` 1     = n
+    n `choose` 2     = 2*n*(n-1)
     n `choose` k     = n `choose` (n-k) when 0<=k<=n
     n `choose` k     = (-1)^k * ((k-n-1) `choose` k)
     n `choose` (k+1) = (n `choose` k) * ((n-k) / (k+1))
@@ -84,29 +85,29 @@ binomial :: (Integral a) => a -> a -> a
     Int -> Int -> Int
     #-}
 binomial n k_
-    | n  <  0   = 0 -- N.B., @binomial_naive 0 0 == 1@
-    | k_ <= 0   = if k_ == 0 then 1 else 0
-    | k_ >= n   = if k_ == n then 1 else 0
-    | otherwise =
-        foldl'
-            (\acc prime -> step acc (fromIntegral prime))
-            1
-            (takeWhile (fromIntegral n >=) primes)
+    | n `seq` k_`seq` False = undefined
+    | 0 < k_ && k_ < n = 
+        k `seq` nk `seq` sqrtN `seq`
+            foldl'
+                (\acc prime -> step acc (fromIntegral prime))
+                1
+                (takeWhile (fromIntegral n >=) primes)
         -- BUG: 'takeWhile' isn't a good producer, so we shouldn't
         -- just @map fromIntegral@. But 'take' is a good producer,
         -- so why isn't 'takeWhile'?
+    | 0 <= k_ && k_ <= n = 1 -- N.B., @binomial_naive 0 0 == 1@
+    | otherwise          = 0
     where
-    -- TODO: is it faster to replace (`quot` 2) by (`shiftR` 1) ?
-    -- TODO: since we know the second operand to quot/rem is positive,
-    -- we should use quotInt/remInt directly to avoid the extra
-    -- tests (if they're not optimized away); for Integer the only
-    -- extra test is against 0, but maybe we should call
-    -- quotInteger/remInteger directly too.
+    -- TODO: since we know the second operands to quot/rem are
+    -- positive, we should use quotInt/remInt directly to avoid the
+    -- extra tests (the overflow errors are not optimized away).
+    
     k     = fromIntegral $! if k_ > n `quot` 2 then n - k_ else k_
     nk    = n - k
     sqrtN = floor (sqrt (fromIntegral n) :: Double) `asTypeOf` n
 
     step acc prime
+        | acc `seq` prime `seq` False = undefined
         | prime > nk         = acc * prime
         | prime > n `quot` 2 = acc
         | prime > sqrtN      =
@@ -116,88 +117,21 @@ binomial n k_
         | otherwise = acc * go n k 0 1
         where
         go n' k' r p
-            | n' > 0 =
-                if n' `rem` prime < (k' `rem` prime) + r
-                then go (n' `quot` prime) (k' `quot` prime) 1 $! p * prime
-                else go (n' `quot` prime) (k' `quot` prime) 0 p
-            | otherwise = p
+            | n' `seq` k' `seq` r `seq` p `seq` False = undefined
+            | n' <= 0   = p
+            | n' `rem` prime < (k' `rem` prime) + r
+                        = go (n' `quot` prime) (k' `quot` prime) 1 $! p * prime
+            | otherwise = go (n' `quot` prime) (k' `quot` prime) 0 p
+            
         {- -- BENCH: apparently this is an unreliable optimization.
         | otherwise = acc * (prime ^ go n k 0 0)
         where
         go n' k' r p
-            | n' > 0 =
-                if n' `rem` prime < (k' `rem` prime) + r
-                then go (n' `quot` prime) (k' `quot` prime) 1 $! p+1
-                else go (n' `quot` prime) (k' `quot` prime) 0 p
-            | otherwise = p `asTypeOf` acc
+            | n' <= 0   = p `asTypeOf` acc
+            | n' `rem` prime < (k' `rem` prime) + r
+                        = go (n' `quot` prime) (k' `quot` prime) 1 $! p+1
+            | otherwise = go (n' `quot` prime) (k' `quot` prime) 0 p
         -}
-
-
-{-
--- | Fast binomial. From Peter Luschny 2010-02-01, LGPL 2.1 or CC-BY-SA 3.0.
-binomial_parallel n k
-    | k < 0  = 0
-    | k > n  = 0
-    | k == 0 = 1
-    | k == n = 1
-    | otherwise = do
-        let fi     = 0
-        let k'     = if k > n / 2 then n - k else k
-        let nk     = n - k'
-        let sqrtN  = floor (sqrt n)
-        let primes = takeWhile (n >=) primes
-        --
-        forM primes $ \prime -> do
-            when (prime > nk) $ do
-                primes[fi++] := prime
-                continue
-            when (prime > n / 2) $ do
-                continue
-            when (prime > sqrtN) $ do
-                when (n `mod` prime < k `mod` prime) $ do
-                    primes[fi++] := prime
-                continue
-            let r  = 0
-            let n' = n
-            let k'' = k'
-            let p = 1
-            while (n' > 0) $ do
-                r := (n' `mod` prime) < (k'' `mod` prime + r) ? 1 : 0
-                when (r==1) (p :*= prime)
-                n'  :/= prime
-                k'' :/= prime
-            when (p > 1) (primes[fi++] := p)
-        --
-        productOf primes 0 fi
-
--- We only do the first part (converting Int to Long) if the array is of ints.
--- This builds @b@ in a way that ensures that operands are of approximately equal size (because that's faster), but the recursive call doesn't. Why?
-productOf _ _ 0 = 1
-productOf a start length =
-    len := (length + 1) / 2
-    b := new long[len]
-    for (int k := 0, int i := start, int j := start + length - 1
-        ; i < j
-        ; ++i, ++j, --k
-        )
-        b[k] := a[i] * (long)a[j]
-    if (i == j) b[k++] := a[j]
-    if (k > PARALLEL_THRESHOLD)
-        then
-            future <- recProduct b ((k - 1) / 2 + 1) (k - 1)
-            left =  recProduct b 0 ((k - 1) / 2)
-            right <- future
-            return (left * right)
-        else
-            recProduct b 0 (k-1)
-
-recProduct arr n m
-    | n >  m    = 1
-    | n == m    = arr !! n
-    | otherwise =
-        let k = (n + m) `shiftR` 1
-        in recProduct s n k * recProduct s (k + 1) m
--}
 
 ----------------------------------------------------------------
 ----------------------------------------------------------- fin.
--- test/bench/Math/Combinatorics/BinomialBench.hs
+++ test/bench/Math/Combinatorics/BinomialBench.hs
@@ -32,10 +32,15 @@ main =
                 C.bgroup (show k)
                     -- See bug note.
                     -- [ C.bench "binomial_0" (C.whnf (binomial_0 n) k)
-                    [ C.bench "binomial_1" (C.whnf (binomial_1 n) k)
-                    , C.bench "binomial_2" (C.whnf (binomial_2 n) k)
-                    , C.bench "binomial_3" (C.whnf (binomial_3 n) k)
-                    , C.bench "binomial_4" (C.whnf (binomial_4 n) k)
+                    [ C.bench "binomial_1"    (C.whnf (binomial_1    n) k)
+                    , C.bench "binomial_2"    (C.whnf (binomial_2    n) k)
+                    , C.bench "binomial_25"   (C.whnf (binomial_25   n) k)
+                    , C.bench "binomial_250"  (C.whnf (binomial_250  n) k)
+                    , C.bench "binomial_2501" (C.whnf (binomial_2501 n) k)
+                    , C.bench "binomial_2502" (C.whnf (binomial_2502 n) k)
+                    , C.bench "binomial_25015" (C.whnf (binomial_25015 n) k)
+                    , C.bench "binomial_251"  (C.whnf (binomial_251  n) k)
+                    , C.bench "binomial_3"    (C.whnf (binomial_3    n) k)
                     ]
     where
     for = flip map
@@ -92,6 +97,7 @@ binomial_0 n k
 -- <http://www.polyomino.f2s.com/david/haskell/hs/CombinatoricsCounting.hs.txt>
 
 
+----------------------------------------------------------------
 -- | The starting efficient implementation.
 binomial_1 :: (Integral a) => a -> a -> a
     -- The result type could be any (Num b) if desired.
@@ -100,6 +106,7 @@ binomial_1 :: (Integral a) => a -> a -> a
     Int -> Int -> Int
     #-}
 binomial_1 n k_
+    | n  <  0   = 0 -- N.B., @binomial_naive 0 0 == 1@
     | k_ <  0   = 0
     | k_ >  n   = 0
     | k_ == 0   = 1
@@ -131,6 +138,7 @@ binomial_1 n k_
             | otherwise = p
 
 
+----------------------------------------------------------------
 -- | Oddly, at -O1, factoring out the basis conditions makes it slower for
 -- small @n@ with extremal @k@, though it gets faster for large
 -- @n@. The differences are marginal in any case (+/- 1--3%). With -O2 this starts reliably outperforming 'binomial_1', however the difference is still marginal.
@@ -141,6 +149,7 @@ binomial_2 :: (Integral a) => a -> a -> a
     Int -> Int -> Int
     #-}
 binomial_2 n k_
+    | n  <  0   = 0 -- N.B., @binomial_naive 0 0 == 1@
     | k_ <= 0   = if k_ == 0 then 1 else 0
     | k_ >= n   = if k_ == n then 1 else 0
     | otherwise =
@@ -170,6 +179,242 @@ binomial_2 n k_
             | otherwise = p
 
 
+----------------------------------------------------------------
+-- | A different factorization of the basis cases. At @-O1@ this is consistently (though not universally) better than 'binomial_2', though it's a toss up between 'binomial_1' and this one. At @-O2@ it seems generally better than both.
+binomial_25 :: (Integral a) => a -> a -> a
+    -- The result type could be any (Num b) if desired.
+{-# SPECIALIZE binomial_25 ::
+    Integer -> Integer -> Integer,
+    Int -> Int -> Int
+    #-}
+binomial_25 n k_
+    | 0 < k_ && k_ < n = 
+        foldl'
+            (\acc prime -> step acc (fromIntegral prime))
+            1
+            (takeWhile (fromIntegral n >=) primes)
+            
+    | 0 <= k_ && k_ <= n = 1
+    | otherwise          = 0
+    where
+    k     = fromIntegral $! if k_ > n `quot` 2 then n - k_ else k_
+    nk    = n - k
+    sqrtN = floor (sqrt (fromIntegral n) :: Double) `asTypeOf` n
+
+    step acc prime
+        | prime > nk         = acc * prime
+        | prime > n `quot` 2 = acc
+        | prime > sqrtN      =
+            if n `rem` prime < k `rem` prime
+            then acc * prime
+            else acc
+        | otherwise = acc * go n k 0 1
+        where
+        go n' k' r p
+            | n' > 0 =
+                if n' `rem` prime < (k' `rem` prime) + r
+                then go (n' `quot` prime) (k' `quot` prime) 1 $! p * prime
+                else go (n' `quot` prime) (k' `quot` prime) 0 p
+            | otherwise = p
+
+----------------------------------------------------------------
+-- | A prettier variation on the guards of @go@, and it's faster too!
+binomial_250 :: (Integral a) => a -> a -> a
+    -- The result type could be any (Num b) if desired.
+{-# SPECIALIZE binomial_250 ::
+    Integer -> Integer -> Integer,
+    Int -> Int -> Int
+    #-}
+binomial_250 n k_
+    | 0 < k_ && k_ < n = 
+        foldl'
+            (\acc prime -> step acc (fromIntegral prime))
+            1
+            (takeWhile (fromIntegral n >=) primes)
+            
+    | 0 <= k_ && k_ <= n = 1
+    | otherwise          = 0
+    where
+    k     = fromIntegral $! if k_ > n `quot` 2 then n - k_ else k_
+    nk    = n - k
+    sqrtN = floor (sqrt (fromIntegral n) :: Double) `asTypeOf` n
+
+    step acc prime
+        | prime > nk         = acc * prime
+        | prime > n `quot` 2 = acc
+        | prime > sqrtN      =
+            if n `rem` prime < k `rem` prime
+            then acc * prime
+            else acc
+        | otherwise = acc * go n k 0 1
+        where
+        go n' k' r p
+            | n' <= 0   = p
+            | n' `rem` prime < (k' `rem` prime) + r
+                        = go (n' `quot` prime) (k' `quot` prime) 1 $! p * prime
+            | otherwise = go (n' `quot` prime) (k' `quot` prime) 0 p
+
+
+----------------------------------------------------------------
+-- | It seems 'binomial_250' isn't strict in @n@, and @go@ not strict in @r@; let's fix that.
+binomial_2501 :: (Integral a) => a -> a -> a
+    -- The result type could be any (Num b) if desired.
+{-# SPECIALIZE binomial_2501 ::
+    Integer -> Integer -> Integer,
+    Int -> Int -> Int
+    #-}
+binomial_2501 n k_
+    | n `seq` k_`seq` False = undefined -- for strictness analysis
+    | 0 < k_ && k_ < n = 
+        k `seq` nk `seq` sqrtN `seq` -- for strictness analysis
+            foldl'
+                (\acc prime -> step acc (fromIntegral prime))
+                1
+                (takeWhile (fromIntegral n >=) primes)
+    | 0 <= k_ && k_ <= n = 1
+    | otherwise          = 0
+    where
+    k     = fromIntegral $! if k_ > n `quot` 2 then n - k_ else k_
+    nk    = n - k
+    sqrtN = floor (sqrt (fromIntegral n) :: Double) `asTypeOf` n
+
+    step acc prime
+        | acc `seq` prime `seq` False = undefined -- for strictness analysis
+        | prime > nk         = acc * prime
+        | prime > n `quot` 2 = acc
+        | prime > sqrtN      =
+            if n `rem` prime < k `rem` prime
+            then acc * prime
+            else acc
+        | otherwise = acc * go n k 0 1
+        where
+        go n' k' r p
+            | n' `seq` k' `seq` r `seq` p `seq` False = undefined -- for strictness analysis
+            | n' <= 0   = p
+            | n' `rem` prime < (k' `rem` prime) + r
+                        = go (n' `quot` prime) (k' `quot` prime) 1 $! p * prime
+            | otherwise = go (n' `quot` prime) (k' `quot` prime) 0 p
+
+
+-- | This version doesn't float out the strictness on what @step@ closes over. It's marginally marginally slower.
+binomial_2502 :: (Integral a) => a -> a -> a
+    -- The result type could be any (Num b) if desired.
+{-# SPECIALIZE binomial_2502 ::
+    Integer -> Integer -> Integer,
+    Int -> Int -> Int
+    #-}
+binomial_2502 n k_
+    | n `seq` k_`seq` False = undefined -- for strictness analysis
+    | 0 < k_ && k_ < n = 
+        foldl'
+            (\acc prime -> step acc (fromIntegral prime))
+            1
+            (takeWhile (fromIntegral n >=) primes)
+    | 0 <= k_ && k_ <= n = 1
+    | otherwise          = 0
+    where
+    k     = fromIntegral $! if k_ > n `quot` 2 then n - k_ else k_
+    nk    = n - k
+    sqrtN = floor (sqrt (fromIntegral n) :: Double) `asTypeOf` n
+
+    step acc prime
+        | k `seq` nk `seq` sqrtN `seq`
+            acc `seq` prime `seq` False = undefined -- for strictness analysis
+        | prime > nk         = acc * prime
+        | prime > n `quot` 2 = acc
+        | prime > sqrtN      =
+            if n `rem` prime < k `rem` prime
+            then acc * prime
+            else acc
+        | otherwise = acc * go n k 0 1
+        where
+        go n' k' r p
+            | n' `seq` k' `seq` r `seq` p `seq` False = undefined -- for strictness analysis
+            | n' <= 0   = p
+            | n' `rem` prime < (k' `rem` prime) + r
+                        = go (n' `quot` prime) (k' `quot` prime) 1 $! p * prime
+            | otherwise = go (n' `quot` prime) (k' `quot` prime) 0 p
+
+
+----------------------------------------------------------------
+-- | Try not closing over @prime@ in @go@ in order to avoid allocation of closures. Unfortunately is marginally slower.
+binomial_25015 :: (Integral a) => a -> a -> a
+    -- The result type could be any (Num b) if desired.
+{-# SPECIALIZE binomial_25015 ::
+    Integer -> Integer -> Integer,
+    Int -> Int -> Int
+    #-}
+binomial_25015 n k_
+    | n `seq` k_`seq` False = undefined -- for strictness analysis
+    | 0 < k_ && k_ < n = 
+        k `seq` nk `seq` sqrtN `seq` -- for strictness analysis
+            foldl'
+                (\acc prime -> step acc (fromIntegral prime))
+                1
+                (takeWhile (fromIntegral n >=) primes)
+    | 0 <= k_ && k_ <= n = 1
+    | otherwise          = 0
+    where
+    k     = fromIntegral $! if k_ > n `quot` 2 then n - k_ else k_
+    nk    = n - k
+    sqrtN = floor (sqrt (fromIntegral n) :: Double) `asTypeOf` n
+
+    step acc prime
+        | acc `seq` prime `seq` False = undefined -- for strictness analysis
+        | prime > nk         = acc * prime
+        | prime > n `quot` 2 = acc
+        | prime > sqrtN      =
+            if n `rem` prime < k `rem` prime
+            then acc * prime
+            else acc
+        | otherwise = acc * go prime n k 0 1
+    
+    go prime n' k' r p
+        | prime `seq` n' `seq` k' `seq` r `seq` p `seq` False = undefined -- for strictness analysis
+        | n' <= 0   = p
+        | n' `rem` prime < (k' `rem` prime) + r
+                    = go prime (n' `quot` prime) (k' `quot` prime) 1 $! p * prime
+        | otherwise = go prime (n' `quot` prime) (k' `quot` prime) 0 p
+
+
+----------------------------------------------------------------
+-- | A prettier variation on the guards of @step@, but unfortunately it's significantly slower due to branch-prediction\/common-case issues.
+binomial_251 :: (Integral a) => a -> a -> a
+    -- The result type could be any (Num b) if desired.
+{-# SPECIALIZE binomial_251 ::
+    Integer -> Integer -> Integer,
+    Int -> Int -> Int
+    #-}
+binomial_251 n k_
+    | 0 < k_ && k_ < n = 
+        foldl'
+            (\acc prime -> step acc (fromIntegral prime))
+            1
+            (takeWhile (fromIntegral n >=) primes)
+            
+    | 0 <= k_ && k_ <= n = 1
+    | otherwise          = 0
+    where
+    k     = fromIntegral $! if k_ > n `quot` 2 then n - k_ else k_
+    nk    = n - k
+    sqrtN = floor (sqrt (fromIntegral n) :: Double) `asTypeOf` n
+
+    step acc prime
+        | prime > nk                    = acc * prime
+        | prime > n `quot` 2            = acc
+        | prime <= sqrtN                = acc * go n k 0 1
+        | n `rem` prime < k `rem` prime = acc * prime
+        | otherwise                     = acc
+        where
+        go n' k' r p
+            | n' > 0 =
+                if n' `rem` prime < (k' `rem` prime) + r
+                then go (n' `quot` prime) (k' `quot` prime) 1 $! p * prime
+                else go (n' `quot` prime) (k' `quot` prime) 0 p
+            | otherwise = p
+
+
+----------------------------------------------------------------
 -- | This seems on par with @binomial_1@ for small @n@, but scales
 -- better. This is expected since exponentiation by '(^)' only
 -- requires /O(log_2 p)/ multiplications instead of /O(p)/, however
@@ -186,15 +431,14 @@ binomial_3 :: (Integral a) => a -> a -> a
     Int -> Int -> Int
     #-}
 binomial_3 n k_
-    | k_ <  0   = 0
-    | k_ >  n   = 0
-    | k_ == 0   = 1
-    | k_ == n   = 1
-    | otherwise =
+    | 0 < k_ && k_ < n = 
         foldl'
             (\acc prime -> step acc (fromIntegral prime))
             1
             (takeWhile (fromIntegral n >=) primes)
+            
+    | 0 <= k_ && k_ <= n = 1
+    | otherwise          = 0
     where
     k     = fromIntegral $! if k_ > n `quot` 2 then n - k_ else k_
     nk    = n - k
@@ -217,42 +461,8 @@ binomial_3 n k_
             | otherwise = p :: Int -- `asTypeOf` acc
 
 
--- | Combine the changes of @binomial_2@ and @binomial_3@.
-binomial_4 :: (Integral a) => a -> a -> a
-    -- The result type could be any (Num b) if desired.
-{-# SPECIALIZE binomial_4 ::
-    Integer -> Integer -> Integer,
-    Int -> Int -> Int
-    #-}
-binomial_4 n k_
-    | k_ <= 0   = if k_ == 0 then 1 else 0
-    | k_ >= n   = if k_ == n then 1 else 0
-    | otherwise =
-        foldl'
-            (\acc prime -> step acc (fromIntegral prime))
-            1
-            (takeWhile (fromIntegral n >=) primes)
-    where
-    k     = fromIntegral $! if k_ > n `quot` 2 then n - k_ else k_
-    nk    = n - k
-    sqrtN = floor (sqrt (fromIntegral n) :: Double) `asTypeOf` n
-
-    step acc prime
-        | prime > nk         = acc * prime
-        | prime > n `quot` 2 = acc
-        | prime > sqrtN      =
-            if n `rem` prime < k `rem` prime
-            then acc * prime
-            else acc
-        | otherwise = acc * (prime ^ go n k 0 0)
-        where
-        go n' k' r p
-            | n' > 0 =
-                if n' `rem` prime < (k' `rem` prime) + r
-                then go (n' `quot` prime) (k' `quot` prime) 1 $! p+1
-                else go (n' `quot` prime) (k' `quot` prime) 0 p
-            | otherwise = p :: Int -- `asTypeOf` acc
-
+----------------------------------------------------------------
+----------------------------------------------------------------
 {-
 binomial_z :: (Integral a) => a -> a -> a
     -- The result type could be any (Num b) if desired.
